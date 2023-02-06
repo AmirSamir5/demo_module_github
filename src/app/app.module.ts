b483b3d5b67d2d5672f6583f1e37b54f1a2b6d2d
Amir Samir <asamir@citglobal.com>
2023-02-06 16:25:47 +0200

    - fix main again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainModule } from './main/main.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, MainModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,5 +5,6 @@ import { MainComponent } from './main.component';
 @NgModule({
   declarations: [MainComponent],
   imports: [CommonModule],
+  exports: [MainComponent],
 })
 export class MainModule {}
