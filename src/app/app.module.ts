260503f6a2e5941be66a7eeb861515b33e2f99eb
Amir Samir <asamir@citglobal.com>
2023-02-06 16:43:38 +0200

    - edit main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MainModule } from './main/main.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MainModule],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
